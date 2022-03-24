cc1f31f320146fa9c5360700cc3b77fac0609739
nahidbd11 <messibossbarca@gmail.com>
2022-03-24 23:28:08 +0600

    2 files modified

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ export default function App() {
           </Father>
         )}
       </GrandFather>
+      <Father />
+      <Son />
     </div>
   );
 }
--- src/Father.js
+++ src/Father.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Son from './Son';
 export default function Father(props) {
-  console.log(props.children.asset);
+  // console.log(props.children.asset);
   const greetingFromFather = () => 'hellow dear son';
   const fatherName = 'nahid';
   return (
     <div>
       Father
-      {props.children({ greetingFromFather, fatherName })}
+      {props.children && props.children({ greetingFromFather, fatherName })}
       {/* <Son /> */}
     </div>
   );
